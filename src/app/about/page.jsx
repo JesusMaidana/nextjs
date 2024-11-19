980568f50736ec22a92e751746bd2e079b3a1b18
Jesús <jesusmaidana35@gmail.com>
2024-11-19 11:15:40 -0300

    Continuacion de proyecto nextjs

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,20 @@
-function AboutPage () {
-    return <h1>About</h1>
-}
+"use client"
+import { useRouter } from "next/navigation";
 
-export default AboutPage;
+export default function AboutPage () {
+    const router = useRouter()
+    return (
+        <section>
+            <h1>About</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus unde error illum ut provident tenetur, distinctio neque et assumenda similique eaque consequuntur reprehenderit illo sed pariatur officia deserunt expedita tempore numquam mollitia animi eius laboriosam ipsa. Exercitationem unde, laborum asperiores officia consectetur, aperiam eius praesentium minima doloribus illo deserunt voluptas aliquam a perspiciatis sequi dignissimos ad ab voluptate repellendus ex velit officiis iste blanditiis? Id totam ea quaerat dolore facilis nihil veritatis eum maxime accusantium, dolorem maiores at asperiores, cumque tenetur architecto ab! Reiciendis asperiores vitae laboriosam quia corporis ducimus soluta blanditiis? Est dolor hic reprehenderit, dolorem consequuntur quos minus.
+            </p>
+            <button className="bg-sky-600 px-3 py-4 rounded-md" onClick={()=>{
+                
+                router.push("/")
+            }}>
+                click
+            </button>
+        </section>
+    );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,7 @@ export default function HomePage () {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <div className="bg-blue-500 text-white p-4">
-  Si ves esto en azul, ¡Tailwind está funcionando!
-</div>
+    
       
     </section>
 }
